782fb8701eb17c95675b9ba878480ac4173c106d
Jaime <texasappz@gmail.com>
2020-05-02 11:53:58 -0500

    Another change by me

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,6 +3,7 @@
         <h1 class="test">Register Page</h1>
         <h5>This is a change added by Phil</h5>
         <h5>This is a change added by Jaime G</h5>
+        <h5>Another change by Jaime</h5>
     </div>
 </template>
 <script>
