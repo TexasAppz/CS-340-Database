542b3aa74710c80726f6a5dc5be1a7a951a3253c
Phillip Cazella <cazellap@oregonstate.edu>
2020-05-02 09:48:14 -0700

    minor change for testing

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,17 +1,17 @@
 <template>
-<div>
-  <h1 class="test">Register Page</h1>
-</div>
+    <div>
+        <h1 class="test">Register Page</h1>
+        <h5>This is a change added by Phil</h5>
+    </div>
 </template>
 <script>
-  export default {
-      name: 'Register'
-  };
+export default {
+    name: 'Register'
+};
 </script>
 
 <style scoped>
-  .test{
+.test {
     color: blue;
-  }
-
+}
 </style>
