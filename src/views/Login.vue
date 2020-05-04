e84db9f6a3ff4eea0a3a9af9880624d526e11fa7
Phillip Cazella <cazellap@oregonstate.edu>
2020-05-04 07:18:15 -0700

    removed the comments left behind from the prev merge removed Sass or Less code from STYLE tags reformated style tag to conform

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,86 +1,36 @@
 <template>
-<!-- <<<<<<< HEAD -->
-  <div class="wrapper">
-  <form class="form-signin">
-    <h2 class="form-signin-heading">Login</h2>
-    <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
-    <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
-    <label class="checkbox">
-      <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Forgot Password?
-    </label>
-    <button class="btn btn-lg btn-primary2 btn-block"  type="submit">Login</button>
-  </form>
-</div>
-<!-- ======= -->
-    <!-- <div>
-        <h1 class="test">Login Page</h1>
-
-        <div>
-            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-                <b-form-group
-                    id="input-group-1"
-                    label="Email address:"
-                    label-for="input-1"
-                    description="We'll never share your email with anyone else."
-                >
-                    <b-form-input
-                        id="input-1"
-                        v-model="form.email"
-                        type="email"
-                        required
-                        placeholder="Enter email"
-                    ></b-form-input>
-                </b-form-group>
-
-                <b-form-group
-                    id="input-group-2"
-                    label="Your Name:"
-                    label-for="input-2"
-                >
-                    <b-form-input
-                        id="input-2"
-                        v-model="form.name"
-                        required
-                        placeholder="Enter name"
-                    ></b-form-input>
-                </b-form-group>
-
-                <b-form-group
-                    id="input-group-3"
-                    label="Food:"
-                    label-for="input-3"
-                >
-                    <b-form-select
-                        id="input-3"
-                        v-model="form.food"
-                        :options="foods"
-                        required
-                    ></b-form-select>
-                </b-form-group>
-
-                <b-form-group id="input-group-4">
-                    <b-form-checkbox-group
-                        v-model="form.checked"
-                        id="checkboxes-4"
-                    >
-                        <b-form-checkbox value="me"
-                            >Check me out</b-form-checkbox
-                        >
-                        <b-form-checkbox value="that"
-                            >Check that out</b-form-checkbox
-                        >
-                    </b-form-checkbox-group>
-                </b-form-group>
-
-                <b-button type="submit" variant="primary">Submit</b-button>
-                <b-button type="reset" variant="danger">Reset</b-button>
-            </b-form>
-            <b-card class="mt-3" header="Form Data Result">
-                <pre class="m-0">{{ form }}</pre>
-            </b-card>
-        </div>
-    </div> -->
-<!-- >>>>>>> 01d111a8b7e9aa78cb7376dd9dcc2442ed1ad3c9 -->
+    <div class="wrapper">
+        <form class="form-signin">
+            <h2 class="form-signin-heading">Login</h2>
+            <input
+                type="text"
+                class="form-control"
+                name="username"
+                placeholder="Email Address"
+                required=""
+                autofocus=""
+            />
+            <input
+                type="password"
+                class="form-control"
+                name="password"
+                placeholder="Password"
+                required=""
+            />
+            <label class="checkbox">
+                <input
+                    type="checkbox"
+                    value="remember-me"
+                    id="rememberMe"
+                    name="rememberMe"
+                />
+                Forgot Password?
+            </label>
+            <button class="btn btn-lg btn-primary2 btn-block" type="submit">
+                Login
+            </button>
+        </form>
+    </div>
 </template>
 <script>
 export default {
@@ -102,94 +52,74 @@ export default {
             ],
             show: true
         };
-    },
-// <<<<<<< HEAD
-  };
-// =======
-//     methods: {
-//         onSubmit(evt) {
-//             evt.preventDefault();
-//             alert(JSON.stringify(this.form));
-//         },
-//         onReset(evt) {
-//             evt.preventDefault();
-//             // Reset our form values
-//             this.form.email = '';
-//             this.form.name = '';
-//             this.form.food = null;
-//             this.form.checked = [];
-//             // Trick to reset/clear native browser form validation state
-//             this.show = false;
-//             this.$nextTick(() => {
-//                 this.show = true;
-//             });
-//         }
-//     }
-// };
-// >>>>>>> 01d111a8b7e9aa78cb7376dd9dcc2442ed1ad3c9
+    }
+};
 </script>
 <style scoped>
-/* <<<<<<< HEAD */
-    .test{
-      color: red;
-    }
+.test {
+    color: red;
+}
 </style>
 <style scoped>
-  body {
-  	background: #eee !important;
-  }
+body {
+    background: #eee !important;
+}
 
-  .wrapper {
-  	margin-top: 80px;
+.wrapper {
+    margin-top: 80px;
     margin-bottom: 80px;
-  }
+}
 
-
-  .form-signin {
+.form-signin {
     max-width: 380px;
     padding: 15px 35px 45px;
     margin: 0 auto;
     background-color: #fff;
-    border: 1px solid rgba(0,0,0,0.1);
+    border: 1px solid rgba(0, 0, 0, 0.1);
+}
+.form-signin-heading,
+.checkbox {
+    margin-bottom: 30px;
+}
 
-    .form-signin-heading,
-  	.checkbox {
-  	  margin-bottom: 30px;
-  	}
+.checkbox {
+    font-weight: normal;
+}
 
-  	.checkbox {
-  	  font-weight: normal;
-  	}
+.form-control {
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
 
-  	.form-control {
-  	  position: relative;
-  	  font-size: 16px;
-  	  height: auto;
-  	  padding: 10px;
-  		@include box-sizing(border-box);
+    /*  This project is not configured to used Sass or Less 
+        as a style sheet rule preprocesor
+        
+        @include box-sizing(border-box);
 
-  		&:focus {
-  		  z-index: 2;
-  		}
-  	}
+        &:focus {
+            z-index: 2;
+        }
+    */
+}
 
-  	input[type="text"] {
-  	  margin-bottom: -1px;
-  	  border-bottom-left-radius: 0;
-  	  border-bottom-right-radius: 0;
-  	}
+input[type='text'] {
+    margin-bottom: -1px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
 
-  	input[type="password"] {
-  	  margin-bottom: 20px;
-  	  border-top-left-radius: 0;
-  	  border-top-right-radius: 0;
-  	}
-  }
-  .btn-primary2 {
+input[type='password'] {
+    margin-bottom: 20px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+
+.btn-primary2 {
     color: #fff;
-    background-color: #17A2B8;
-    border-color: #17A2B8;
-  }
+    background-color: #17a2b8;
+    border-color: #17a2b8;
+}
 /* ======= */
 .test {
     color: red;
@@ -199,5 +129,4 @@ export default {
 .test {
     color: #cccccc;
 }
-/* >>>>>>> 01d111a8b7e9aa78cb7376dd9dcc2442ed1ad3c9 */
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,25 +1,54 @@
 <template>
-  <div class="wrapper">
-  <form class="form-signin">
-    <h2 class="form-signin-heading">Register Here</h2>
-    <input type="text" class="form-control" name="firstName" placeholder="First Name" required="" autofocus="" />
-    <label class="checkbox">
-    </label>
-    <input type="password" class="form-control" name="firstName" placeholder="Last Name" required=""/>
-    <label class="checkbox">
-    </label>
-    <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
-    <label class="checkbox">
-    </label>
-    <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
-    <label class="checkbox">
-    </label>
-    <input type="password" class="form-control" name="passwordAgain" placeholder="Enter Password Again" required=""/>
-    <label class="checkbox">
-    </label>
-    <button class="btn btn-lg btn-primary2 btn-block"  type="submit">Register</button>
-  </form>
-</div>
+    <div class="wrapper">
+        <form class="form-signin">
+            <h2 class="form-signin-heading">Register Here</h2>
+            <input
+                type="text"
+                class="form-control"
+                name="firstName"
+                placeholder="First Name"
+                required=""
+                autofocus=""
+            />
+            <label class="checkbox"> </label>
+            <input
+                type="password"
+                class="form-control"
+                name="firstName"
+                placeholder="Last Name"
+                required=""
+            />
+            <label class="checkbox"> </label>
+            <input
+                type="text"
+                class="form-control"
+                name="username"
+                placeholder="Email Address"
+                required=""
+                autofocus=""
+            />
+            <label class="checkbox"> </label>
+            <input
+                type="password"
+                class="form-control"
+                name="password"
+                placeholder="Password"
+                required=""
+            />
+            <label class="checkbox"> </label>
+            <input
+                type="password"
+                class="form-control"
+                name="passwordAgain"
+                placeholder="Enter Password Again"
+                required=""
+            />
+            <label class="checkbox"> </label>
+            <button class="btn btn-lg btn-primary2 btn-block" type="submit">
+                Register
+            </button>
+        </form>
+    </div>
 </template>
 <script>
 export default {
@@ -33,59 +62,64 @@ export default {
 }
 </style>
 <style scoped>
-  body {
-  	background: #eee !important;
-  }
+body {
+    background: #eee !important;
+}
 
-  .wrapper {
-  	margin-top: 80px;
+.wrapper {
+    margin-top: 80px;
     margin-bottom: 80px;
-  }
+}
 
-
-  .form-signin {
+.form-signin {
     max-width: 380px;
     padding: 15px 35px 45px;
     margin: 0 auto;
     background-color: #fff;
-    border: 1px solid rgba(0,0,0,0.1);
+    border: 1px solid rgba(0, 0, 0, 0.1);
+}
 
-    .form-signin-heading,
-  	.checkbox {
-  	  margin-bottom: 30px;
-  	}
+.form-signin-heading,
+.checkbox {
+    margin-bottom: 30px;
+}
 
-  	.checkbox {
-  	  font-weight: normal;
-  	}
+.checkbox {
+    font-weight: normal;
+}
 
-  	.form-control {
-  	  position: relative;
-  	  font-size: 16px;
-  	  height: auto;
-  	  padding: 10px;
-  		@include box-sizing(border-box);
+.form-control {
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
 
-  		&:focus {
-  		  z-index: 2;
-  		}
-  	}
+    /*  This project is not configured to used Sass or Less 
+        as a style sheet rule preprocesor
+        
+        @include box-sizing(border-box);
 
-  	input[type="text"] {
-  	  margin-bottom: -1px;
-  	  border-bottom-left-radius: 0;
-  	  border-bottom-right-radius: 0;
-  	}
+        &:focus {
+            z-index: 2;
+        }
+    */
+}
 
-  	input[type="password"] {
-  	  margin-bottom: 20px;
-  	  border-top-left-radius: 0;
-  	  border-top-right-radius: 0;
-  	}
-  }
-  .btn-primary2 {
+input[type='text'] {
+    margin-bottom: -1px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
+
+input[type='password'] {
+    margin-bottom: 20px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+
+.btn-primary2 {
     color: #fff;
-    background-color: #17A2B8;
-    border-color: #17A2B8;
-  }
+    background-color: #17a2b8;
+    border-color: #17a2b8;
+}
 </style>
