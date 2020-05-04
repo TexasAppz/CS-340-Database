a761fc63a529e049d1c63c4e4e7591b471c8a97f
Jaime <texasappz@gmail.com>
2020-05-04 08:01:04 -0500

    Home, Login and Register Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,155 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <div>
-          Hello World, This is a div
-        </div>
+    <div class="ex1">
+          <a  font-size="12" href="http://localhost:8080/#/Menu">BREAKFAST</a><a> | </a>
+          <a  href="http://localhost:8080/#/Menu">LUNCH</a><a> | </a>
+          <a href="http://localhost:8080/#/Menu">DINNER</a>
+
+          <link href='https://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister'
+          rel='stylesheet' type='text/css'>
+          <h1>Momma's Place Restaurant</h1>
+          <h2>a Northern California Eatery</h2>
+          <div class="first">
+          <p>Ceaser Salad<span class="price">$14</span><br />
+          <small>romaine lettuce, extra virgin olive oil, garlic, baguette, lemon juice, Parmesan cheese, anchovies, eggs, black pepper, salt</small></p>
+          </div>
+          <span></span>
+
+
+          <div class="second">
+          <p>Fettuccini Alfredo <span class="price">$22</span><br />
+          <small>fettuccine, broccoli florets, garlic, butter, all-purpose flour, milk, grated parmesan, nutmeg, salt, pepper</small></p>
+          </div>
+
+          <div class="dessert">
+          <p>Grilled Stone Fruit Parfait<span class="price">$8</span><br />
+          <small>nectarines (halved), black plums (halved), cherries (pitted and halved), fresh, all- natrual vanilla yogurt</small></p>
+          </div>
+
+
     </div>
 </template>
-
 <script>
 export default {
     name: 'Home',
     components: {}
 };
 </script>
+<style scoped>
+  body {
+    font-family: helvetica, sans-serif;
+    margin: 0 auto;
+    max-width: 600px;
+    background: #232323;
+  }
+  div {
+    height: 200px;
+    background-size: cover;
+    position: relative;
+    margin: 40px 0 0 0;
+    border-radius: 12px;
+  }
+  h1 {
+    font-family: 'Love Ya Like A Sister', cursive;
+    text-align: center;
+    font-size: 75px;
+    color: #aaaaaa;
+    margin: 60px 0 0 0;
+  }
+  h2 {
+    text-align: center;
+    color: #bbbbbb;
+    margin: 0px 0 70px 0;
+  }
+  p {
+    color: rgba(255,255,255,1);
+    background: black;
+    background: linear-gradient(bottom, rgba(0,0,0,1), rgba(0,0,0,.4));
+    background: -webkit-linear-gradient(bottom, rgba(0,0,0,1), rgba(0,0,0,.4));
+    background: -moz-linear-gradient(bottom, rgba(0,0,0,1), rgba(0,0,0,.4));
+    padding: 10px;
+    line-height: 28px;
+    text-align: justify;
+    position: absolute;
+    bottom: 0;
+    margin: 0;
+    height: 30px;
+    transition: height .5s;
+    -webkit-transition: height .5s;
+    -moz-transition: height .5s;
+  }
+
+  small {
+    opacity: 0;
+  }
+
+  .show-description p {
+    height: 150px;
+  }
+
+  .show-description small {
+    opacity: 1;
+  }
+  .first{
+    background-image: url("http://blog.fairwaymarket.com/wp-content/uploads/2013/07/caesar-salad.jpg");
+      margin: auto;
+      width: 50%;
+      padding: 10px;
+  }
+  .second{
+    background-image: url("http://www.budgetbytes.com/wp-content/uploads/2012/02/Broccoli-Fettuccine-close.jpg");
+    margin: auto;
+    width: 50%;
+    padding: 10px;
+  }
+  .dessert{
+    background-image: url("https://3.bp.blogspot.com/-ga7Qrvibqmk/Uc9VCMovdDI/AAAAAAAAFG4/uCr_KFGLWQ0/s500/Grilled+Stone+Fruit+Parfait_Overhead+View-1-5.jpg");
+    margin: auto;
+    width: 50%;
+    padding: 10px;
+  }
+  .price {
+    float: right;
+    font-weight:bold;
+  }
+  @media (max-width: 500px) {
+    h1 {
+      font-size: 50px;
+      margin-top: 20px;
+      line-height: 40px;
+    }
+    h2 {
+      font-size: 20px;
+      margin: 20px 0 30px 0;
+    }
+    a {
+      font-size: 20px;
+      margin: 20px 0 30px 0;
+    }
+    div {
+      margin: 20px 12px 0 12px;
+    }
+    .ex1 {
+      width:10px;
+      margin: auto;
+      border: 3px solid #73AD21;
+    }
+    p {
+      font-size: 20px;
+      line-height: 24px;
+    }
+    small {
+      font-size: 16px;
+    }
+  }
+  div.topBotomBordersOut a:before, div.topBotomBordersOut a:after
+  {
+    position: absolute;
+    left: 0px;
+    width: 100%;
+    height: 2px;
+    background: #FFF;
+    content: "";
+    opacity: 0;
+    transition: all 0.3s;
+  }
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,25 @@
 <template>
-    <div>
-        <h1 class="test">Register Page</h1>
-        <h5>This is a change added by Phil</h5>
-        <h5>This is a change added by Jaime G</h5>
-        <h5>Another change by Jaime</h5>
-    </div>
+  <div class="wrapper">
+  <form class="form-signin">
+    <h2 class="form-signin-heading">Register Here</h2>
+    <input type="text" class="form-control" name="firstName" placeholder="First Name" required="" autofocus="" />
+    <label class="checkbox">
+    </label>
+    <input type="password" class="form-control" name="firstName" placeholder="Last Name" required=""/>
+    <label class="checkbox">
+    </label>
+    <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+    <label class="checkbox">
+    </label>
+    <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+    <label class="checkbox">
+    </label>
+    <input type="password" class="form-control" name="passwordAgain" placeholder="Enter Password Again" required=""/>
+    <label class="checkbox">
+    </label>
+    <button class="btn btn-lg btn-primary2 btn-block"  type="submit">Register</button>
+  </form>
+</div>
 </template>
 <script>
 export default {
@@ -16,4 +31,61 @@ export default {
 .test {
     color: blue;
 }
+</style>
+<style scoped>
+  body {
+  	background: #eee !important;
+  }
+
+  .wrapper {
+  	margin-top: 80px;
+    margin-bottom: 80px;
+  }
+
+
+  .form-signin {
+    max-width: 380px;
+    padding: 15px 35px 45px;
+    margin: 0 auto;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,0.1);
+
+    .form-signin-heading,
+  	.checkbox {
+  	  margin-bottom: 30px;
+  	}
+
+  	.checkbox {
+  	  font-weight: normal;
+  	}
+
+  	.form-control {
+  	  position: relative;
+  	  font-size: 16px;
+  	  height: auto;
+  	  padding: 10px;
+  		@include box-sizing(border-box);
+
+  		&:focus {
+  		  z-index: 2;
+  		}
+  	}
+
+  	input[type="text"] {
+  	  margin-bottom: -1px;
+  	  border-bottom-left-radius: 0;
+  	  border-bottom-right-radius: 0;
+  	}
+
+  	input[type="password"] {
+  	  margin-bottom: 20px;
+  	  border-top-left-radius: 0;
+  	  border-top-right-radius: 0;
+  	}
+  }
+  .btn-primary2 {
+    color: #fff;
+    background-color: #17A2B8;
+    border-color: #17A2B8;
+  }
 </style>
