2a6419e6979479b05353d200c03cc29fbe1fd18f
Phillip Cazella <cazellap@oregonstate.edu>
2020-06-06 12:59:32 -0700

    set cart icon to always show

--- client/src/App.vue
+++ client/src/App.vue
@@ -156,7 +156,7 @@ export default {
             return '';
         },
         showCartIcon() {
-            return store.state.showCartIcon;
+            return true; //store.state.showCartIcon;
         }
     },
     asyncComputed: {
